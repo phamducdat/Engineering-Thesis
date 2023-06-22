0c669dad221bf9f667d1856083975b72504a9b86
phamducdat <phamducdat2402@gmail.com>
2023-06-23 05:14:02 +0700

    fix: delete AccountRealm if it doesn't exist in Keycloak's data

--- src/main/java/com/hust/datpd/engineeringthesis/controller/AccountController.java
+++ src/main/java/com/hust/datpd/engineeringthesis/controller/AccountController.java
@@ -4,6 +4,8 @@ import com.hust.datpd.engineeringthesis.dto.AccountDto;
 import com.hust.datpd.engineeringthesis.message.ErrorResponse;
 import com.hust.datpd.engineeringthesis.service.AccountRealmService;
 import com.hust.datpd.engineeringthesis.validator.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/external/v1/accounts")
 public class AccountController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
 
     final AccountRealmService accountRealmService;
     final ValidatorUtil validatorUtil;
@@ -30,6 +34,13 @@ public class AccountController {
             );
         }
         AccountDto to = accountRealmService.findByUsername(username);
+        if (!validatorUtil.realmExists(to.getRealm())) {
+            accountRealmService.deleteAccountRealmByRealmId(to.getRealm());
+            LOGGER.info("Delete " + to.getRealm() + " in AccountRealmTable because Keycloak's database doesn't contain this id");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ErrorResponse("Không tồn tại tài khoản, vui lòng kiểm tra lại tên tài khoản!")
+            );
+        }
 
         return ResponseEntity.ok(to);
     }
