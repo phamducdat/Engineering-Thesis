e386dde32d2cdcc4b0911aa4751e523fef533c73
phamducdat <phamducdat2402@gmail.com>
2023-07-01 21:26:12 +0700

    feat: create digital certificate

---
+++ client/src/api/system/index.tsx
@@ -0,0 +1,69 @@
+import DP_axios, {CustomAxiosRequestConfig} from "../../custom/axios";
+
+export const copyBrowserAuthentication = async (newName: string) => {
+    const response = await DP_axios.post("/admin/realms/master/authentication/flows/browser/copy", {
+        newName: newName
+    })
+    return response.data || []
+}
+
+
+export const addExecution = async (newName: string) => {
+    const config: CustomAxiosRequestConfig = {captureLocationHeader: true}
+    const response = await DP_axios.post(`/admin/realms/master/authentication/flows/${newName}/executions/execution`, {
+        provider: "auth-x509-client-username-form"
+    }, config)
+    return response.data || []
+}
+
+
+export const raisePriorityExecution = async (executionId: string) => {
+    const response = await DP_axios.post(`/admin/realms/master/authentication/executions/${executionId}/raise-priority`,
+        {"realm": "master", "execution": executionId})
+    return response.data || []
+}
+
+export const lowerPriorityExecution = async (executionId: string) => {
+    const response = await DP_axios.post(`/admin/realms/master/authentication/executions/${executionId}/lower-priority`,
+        {"realm": "master", "execution": executionId})
+    return response.data || []
+}
+
+export const getExecutionById = async (executionId: string) => {
+    const response = await DP_axios.get(`/admin/realms/master/authentication/executions/${executionId}`)
+    return response.data || []
+}
+
+export const configAuthentication = async (executionId: string) => {
+    const config: CustomAxiosRequestConfig = {captureLocationHeader: true}
+    const response = await DP_axios.post(`/admin/realms/master/authentication/executions/${executionId}/config`,
+        {
+            "config": {
+                "x509-cert-auth.mapping-source-selection": "Subject's e-mail",
+                "x509-cert-auth.canonical-dn-enabled": "false",
+                "x509-cert-auth.serialnumber-hex-enabled": "false",
+                "x509-cert-auth.regular-expression": "(.*?)(?:$)",
+                "x509-cert-auth.mapper-selection": "Username or Email",
+                "x509-cert-auth.mapper-selection.user-attribute-name": "usercertificate",
+                "x509-cert-auth.timestamp-validation-enabled": "true",
+                "x509-cert-auth.crl-checking-enabled": "",
+                "x509-cert-auth.crldp-checking-enabled": "false",
+                "x509-cert-auth.crl-relative-path": "crl.pem",
+                "x509-cert-auth.ocsp-checking-enabled": "",
+                "x509-cert-auth.ocsp-fail-open": "false",
+                "x509-cert-auth.confirmation-page-disallowed": "",
+                "x509-cert-auth.revalidate-certificate-enabled": "",
+                "x509-cert-auth.certificate-policy-mode": "All"
+            }, "alias": "x509 config"
+        }, config)
+    return response.data || []
+}
+
+export const updateExecutionById = async (flowName:string, executionId: string) => {
+    const response = await DP_axios.put(`/admin/realms/master/authentication/flows/${flowName}/executions`, {
+        index: 3,
+        id: executionId,
+        requirement: "ALTERNATIVE"
+    })
+    return response.data || []
+}
--- client/src/pages/system/settings/authentication/index.tsx
+++ client/src/pages/system/settings/authentication/index.tsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button} from "antd";
+import {
+    addExecution,
+    configAuthentication,
+    copyBrowserAuthentication,
+    raisePriorityExecution,
+    updateExecutionById
+} from "../../../../api/system";
+import {getRealmInfoByRealmId, updateRealmByRealmId} from "../../../../api/realms";
 
 
 const Authentication: React.FC = () => {
+
+    const [alias, setAlias] = useState(makeid())
+
+
+    const onClick = () => {
+        copyBrowserAuthentication(alias).then(() => {
+            addExecution(alias).then((response) => {
+                const location = response.location
+                const executionId = location.substring(location.lastIndexOf('/') + 1)
+
+                raisePriorityExecution(executionId).then((response) => {
+
+                })
+
+                configAuthentication(executionId).then((response) => {
+                    const location = response.location
+                    const configId = location.substring(location.lastIndexOf('/') + 1)
+
+                })
+
+                updateExecutionById(alias, executionId).then(() => {
+
+                })
+
+                getRealmInfoByRealmId("master").then((response) => {
+                    updateRealmByRealmId("master", {
+                        ...response,
+                        browserFlow: alias
+                    }).then(r => {
+
+                    })
+                })
+            })
+        })
+    }
+
+    function makeid() {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < 10) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return result;
+    }
+
     return (
         <div>
-            Authentication
+            <Button onClick={onClick}>
+                Xác thực bằng chứng chỉ số
+            </Button>
         </div>
     );
 };
