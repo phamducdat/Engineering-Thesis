714037ea1d61b54b63fc60eac6ed05ecd669a0d4
phamducdat <phamducdat2402@gmail.com>
2023-07-27 01:47:17 +0700

    feat: add filter permission transfer

--- client/src/pages/managers/domain/details/PermissionTransfer.tsx
+++ client/src/pages/managers/domain/details/PermissionTransfer.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {createClientUsers, deleteClientUsers, getClientUsersByClientId} from "../../../../api/external";
 import {TransferDirection} from "antd/es/transfer";
-import {Transfer} from "antd";
+import {Col, Form, Row, Select, Tag, Transfer} from "antd";
 import {getUsers} from "../../../../api/user";
 import {getMe} from "../../../../api/admin";
 import {useRootContext} from "../../../root/context/useRootContext";
@@ -19,6 +19,7 @@ export const PermissionTransfer: React.FC<{}> = props => {
     const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
     const [dataSource, setDataSource] = useState<RecordType[]>()
     const [flag, setFlag] = useState(false)
+    const [statusUser, setStatusUser] = useState('active')
     const {realmId, domainId} = useParams()
     const {setSpinning} = useRootContext()
 
@@ -27,29 +28,53 @@ export const PermissionTransfer: React.FC<{}> = props => {
         setFlag(false)
         setSpinning(true)
 
+
+        await getUsers(realmId).then((response) => {
+            const convertData = response
+                .filter((data: any) => {
+                    if (statusUser === 'active')
+                        return data.id !== getMe().sub && data.enabled === true
+                    else
+                        return data.id !== getMe().sub
+                })
+                .map((item: any) => {
+                    if (!item.enabled)
+                        return {
+                            key: item.id,
+                            title:
+                                <>
+                                    {item.username} - <Tag>
+                                    Không hoạt động
+                                </Tag>
+                                </>
+                        }
+                    else
+                        return {
+                            key: item.id,
+                            title: item.username,
+                        }
+                })
+            setDataSource(convertData)
+        })
+
         await getClientUsersByClientId(realmId, domainId).then((res) => {
             setTargetKeys(res.userIds)
         }).finally(() => {
             setFlag(true)
         })
 
-        await getUsers(realmId).then((response) => {
-            const convertData = response
-                .filter((data: any) => data.id !== getMe().sub)
-                .map((item: any) => {
-                    return {
-                        key: item.id,
-                        title: item.username,
-                    }
-                })
-            setDataSource(convertData)
-        })
+
         setSpinning(false)
     }
 
     useEffect(() => {
         getData();
     }, [])
+
+
+    useEffect(() => {
+        getData()
+    }, [statusUser])
 
 
     const onChange = (nextTargetKeys: string[], direction: TransferDirection, moveKeys: string[]) => {
@@ -80,20 +105,53 @@ export const PermissionTransfer: React.FC<{}> = props => {
 
     return (
         <>
-            {flag && dataSource !== undefined && <Transfer
-                dataSource={dataSource}
-                listStyle={{
-                    width: 300,
-                    height: 300,
-                }}
-                titles={['Danh sách tài khoản', 'Phân quyền']}
-                targetKeys={targetKeys}
-                selectedKeys={selectedKeys}
-                onChange={onChange}
-                onSelectChange={onSelectChange}
-                render={(item) => item.title}
-                showSearch
-            />}
+            {flag && dataSource !== undefined &&
+
+                <>
+                    <Row>
+                        <Col span={5}>
+                            <Form.Item
+                                label={"Trạng thái nhân sự"}
+                            >
+                                <Select
+                                    style={{width: "100%"}}
+                                    value={statusUser}
+                                    onChange={(value) => {
+                                        setStatusUser(value)
+                                    }}
+                                    options={[
+                                        {
+                                            label: 'Hoạt động',
+                                            key: 'active',
+                                            value: 'active',
+                                        },
+                                        {
+                                            label: 'Toàn bộ',
+                                            key: 'all',
+                                            value: 'all',
+                                        }
+                                    ]
+                                    }
+                                />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Transfer
+                        dataSource={dataSource}
+                        listStyle={{
+                            width: 300,
+                            height: 300,
+                        }}
+                        titles={['Danh sách tài khoản', 'Phân quyền']}
+                        targetKeys={targetKeys}
+                        selectedKeys={selectedKeys}
+                        onChange={onChange}
+                        onSelectChange={onSelectChange}
+                        render={(item) => item.title}
+                        showSearch
+                    />
+                </>
+            }
         </>
     );
 };
