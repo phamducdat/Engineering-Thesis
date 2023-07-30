863ef16273e285dff8b7c2b4b46c98b72e5f4e8b
phamducdat <phamducdat2402@gmail.com>
2023-07-30 22:19:03 +0700

    fix: update convertWarningMessage

--- client/src/custom/axios/util.tsx
+++ client/src/custom/axios/util.tsx
@@ -1,11 +1,11 @@
 export const convertWarningMessage = (from: string) => {
     switch (from) {
         case 'User exists with same username or email':
-            return 'Đã tồn tại username hoặc email';
+            return 'Đã tồn tại Tên tài khoản hoặc Email';
         case 'User exists with same username':
-            return 'Đã tồn tại username'
+            return 'Đã tồn tại Tên tài khoản'
         case 'User exists with same email':
-            return 'Đã tồn tại email'
+            return 'Đã tồn tại Email'
         default:
             return from;
     }
